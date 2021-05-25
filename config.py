c8c2464f027639f8bd514dcf71b241fdd21a437c
Hang Guo <hangguowork@gmail.com>
2021-05-25 12:23:48 -0700

    Development 05/25/2021:    fix pep8 issues.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DEBUG = True
 # Connect to the database
 
 #IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://gdmtenfbaugfep:923f2099a60d14c74f504514963c345d84a33509d799a8e2ea2ce3381820f6cc@ec2-54-160-96-70.compute-1.amazonaws.com:5432/dgfjususe35e5'
+SQLALCHEMY_DATABASE_URI = ''
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 Customer = ""
 Admin = ""
